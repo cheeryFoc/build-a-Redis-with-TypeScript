a8060a91bf0dc4d0a06333fa9210363837a5cb00
cheery <cheerybill@gmail.com>
2024-06-18 14:41:06 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as net from "net";
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+
+// Uncomment this block to pass the first stage
+const server: net.Server = net.createServer((connection: net.Socket) => {
+    connection.on("data", (data) => {
+        console.log("Received:", JSON.stringify(data.toString()));
+        connection.write("+PONG\r\n");
+    });
+});
+
+server.listen(6379, "127.0.0.1");
