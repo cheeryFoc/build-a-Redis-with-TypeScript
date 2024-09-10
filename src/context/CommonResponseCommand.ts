14828635cbb07f91ab5e98423a8950206f394fed
cheery <cheerybill@gmail.com>
2024-09-10 21:04:41 +0800

    get & set

--- src/context/CommonResponseCommand.ts
+++ src/context/CommonResponseCommand.ts
@@ -2,4 +2,5 @@ export enum CommonResponse {
     COMMAND_NOT_FOUND = "COMMAND_NOT_FOUND",
     Pong = "PONG",
     ECHO = "ECHO",
+    OK = "OK"
 }
--- src/service/impl/AnalysisService.ts
+++ src/service/impl/AnalysisService.ts
@@ -1,12 +1,17 @@
 import { CommonResponse } from '@/context/CommonResponseCommand';
 import { Result } from '@/domain/Result';
 import { RedisParser } from '@/service/RedisParser';
+import { log } from 'node:console';
 export class AnalysisService implements RedisParser<string> {
 
     encodedRedisResponse(data: Buffer): string {
 
+        const container = new Map();
+
         try {
             const [command, ...args] = this.formatDate(data);
+
+            // console.log(args)
 
             switch (command.toLowerCase()) {
                 case 'ping':
@@ -14,16 +19,19 @@ export class AnalysisService implements RedisParser<string> {
                 case 'echo':
                     return `$${args[0].length}\r\n${args[0]}\r\n`;
                 case 'set':
-                //
+                    container.set(args[0], args[1]);
+                    return Result.ok(CommonResponse.OK);
                 case 'get':
-                //
+                    console.log(args[0]);
+                    const value = container.get(`${args[0]}`);
+                    console.log(value);
+                    return `$${value ? value.length + '\r\n' + value : '-1'}\r\n`;
                 default:
                     return Result.ok(CommonResponse.COMMAND_NOT_FOUND);
             }
-            
+
         } catch (error) {
             console.error("Error processing data:", error);
-        } finally {
             return Result.error("Processing error");
         }
 
